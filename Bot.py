4318bad07fddcda8833bacd6965dfe830d6e6d7f
seb <sebyzy@gmail.com>
2021-12-31 13:20:48 -0500

    12.31 update

--- Bot.py
+++ Bot.py
@@ -6,22 +6,183 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 
+
+
+NRML = "https://nrml.ca/products/"
 PATH = "/Users/seb/Chromedriver/chromedriver"
 driver = webdriver.Chrome(PATH)
 
- 
-#auto checkout NRML
-driver.get("https://nrml.ca/products/w-nike-dunk-low-se-dd7099-001")
-add_to_cart = driver.find_element_by_class_name("add-to-cart")
-ActionChains(driver).move_to_element(add_to_cart).click(add_to_cart).perform()
-try:
-    WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div[data-testid='ShopifyPay-button'][role='button']"))).click()
-    
-finally:
-    print("DONEEEEEEEEE")
-    driver.quit() 
 
+def Generate_Early_Link(s, site):
+    link = ""
+    for char in s: 
+        if(char == " "):
+            link += "-"
+        elif(char.isalpha() or char. isnumeric()):
+            link += char.lower() 
+    return site+link
+
+confirm = input("NRML? Y/N")
+if(confirm.lower() == "y"):
+    CALENDAR_LINK = "https://nrml.ca/blogs/release-calendar" 
+    driver.get(CALENDAR_LINK)
+    calendar = []
+    release_blog = driver.find_elements_by_class_name("font-heading")
+    for shoes in release_blog:
+        kicks = shoes.text
+        if(kicks == "NEWSLETTER"):
+            break
+        elif(kicks != "" and kicks != "RELEASE CALENDAR"):
+            kicks = kicks.strip()
+            calendar.append(kicks)
+    for shoes in calendar:
+        print(Generate_Early_Link(shoes, NRML))
+    driver.quit()    
+elif(confirm.lower() == "n"):
+    print("exit")
+    exit
+else:
+    print("Error")
+    exit     
+
+
+
+
+
+# confirm = input("NRML? Y/N")
+# if(confirm.lower() == "y"):
+#     CALENDAR_LINK = "https://nrml.ca/blogs/release-calendar" 
+#     driver.get(CALENDAR_LINK)
+#     calendar = []
+#     release_blog = driver.find_elements_by_class_name("font-heading")
+#     for shoes in release_blog:
+#         kicks = shoes.text
+#         if(kicks == "NEWSLETTER"):
+#             break
+#         elif(kicks != "" and kicks != "RELEASE CALENDAR"):
+#             print(kicks)
+#             calendar.append(kicks)
+
+#     driver.quit()    
+# elif(confirm.lower() == "n"):
+#     print("exit")
+#     exit
+# else:
+#     print("Error")
+#     exit       
+
+
+
+
+
+# #release calendar
+# CALENDAR_LINK = "https://nrml.ca/blogs/release-calendar" 
+# driver.get(CALENDAR_LINK)
+# calendar = []
+# release_blog = driver.find_elements_by_class_name("font-heading")
+# for shoes in release_blog:
+#     kicks = shoes.text
+#     if(kicks == "NEWSLETTER"):
+#         break
+#     elif(kicks != "" and kicks != "RELEASE CALENDAR"):
+#         print(kicks)
+#         calendar.append(kicks)
+# driver.quit()    
+
+  
+# driver.get(LOG_IN)
+# email = driver.find_element_by_id("CustomerEmail")
+# email.send_keys(EMAIL)
+# password = driver.find_element_by_id("CustomerPassword")
+# password.send_keys(PASSWORD)
+# submit = driver.find_element_by_class_name("btn")
+# ActionChains(driver).move_to_element(submit).click(submit).perform()
+# driver.refresh()
+#driver.get(LINK)
+# boo = True
+# while boo:
+#     try:
+#         add_to_cart = driver.find_element_by_class_name("add-to-cart")
+#         ActionChains(driver).move_to_element(add_to_cart).click(add_to_cart).perform()
+#         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div[data-testid='ShopifyPay-button'][role='button']"))).click()
+#         boo = False
+#     except:
+#         driver.refresh()
+
+
+# #log in
+# EMAIL = "sebyzy@gmail.com"
+# PASSWORD = 
+# LOG_IN = "https://nrml.ca/account"
+# LINK = "https://nrml.ca/products/air-jordan-1-retro-high-og-555088-063" #"https://nrml.ca/products/w-nike-dunk-low-se-dd7099-001"
+
+
+# driver.get(LOG_IN)
+# email = driver.find_element_by_id("CustomerEmail")
+# email.send_keys(EMAIL)
+# password = driver.find_element_by_id("CustomerPassword")
+# password.send_keys(PASSWORD)
+# submit = driver.find_element_by_class_name("btn")
+# ActionChains(driver).move_to_element(submit).click(submit).perform()
+# driver.refresh()
+# driver.get(LINK)
+# boo = True
+# while boo:
+#     try:
+#         add_to_cart = driver.find_element_by_class_name("add-to-cart")
+#         ActionChains(driver).move_to_element(add_to_cart).click(add_to_cart).perform()
+#         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div[data-testid='ShopifyPay-button'][role='button']"))).click()
+#         boo = False
+#     except:
+#         driver.refresh()
+
+
+#add_to_cart = driver.find_element_by_class_name("add-to-cart")
+#ActionChains(driver).move_to_element(add_to_cart).click(add_to_cart).perform()
+
+
+# boo = True
+# while boo:
+#     try:
+#         add_to_cart = driver.find_element_by_class_name("add-to-cart")
+#         ActionChains(driver).move_to_element(add_to_cart).click(add_to_cart).perform()
+#         # try:
+#         #     WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div[data-testid='ShopifyPay-button'][role='button']"))).click()
+#         # finally:
+#         #     boo = False
+        
+#     except:
+#         driver.refresh()
+  
+
+
+
+#add_to_cart = driver.find_element_by_class_name("add-to-cart")
+#ActionChains(driver).move_to_element(add_to_cart).click(add_to_cart).perform()
+#WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div[data-testid='ShopifyPay-button'][role='button']"))).click()
+         
+        
+
+ 
+# #auto checkout NRML
+# LINK = "https://nrml.ca/"#"https://nrml.ca/products/air-jordan-1-retro-high-og-555088-063" #"https://nrml.ca/products/w-nike-dunk-low-se-dd7099-001"
+# driver.get(LINK)
+# boo = True
+# while boo:
+#     try:
+#         add_to_cart = driver.find_element_by_class_name("add-to-cart")
+#         ActionChains(driver).move_to_element(add_to_cart).click(add_to_cart).perform()
+#         try:
+#             WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div[data-testid='ShopifyPay-button'][role='button']"))).click()
+#         finally:
+#             boo = False
+        
+#     except:
+#         driver.refresh()
+
+    
 
 
 #_11GJAaBiShTVGYuXppoYvE

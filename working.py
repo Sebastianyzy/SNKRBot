97c69b82e69c09f7a39801ae08249e8a986d2f8f
seb <sebyzy@gmail.com>
2022-01-15 06:25:42 -0500

    q

--- working.py
+++ working.py
@@ -9,3 +9,5 @@ inventory = {1:{"name": "Milk", "price": 3.99, "brand":"regular"}}
 @app.get("/get-item/{item_id}")
 def get_item(item_id: int = Path(None, description="The ID of the item",gt=0,lt=4) ):
     return inventory[item_id]     
+
+

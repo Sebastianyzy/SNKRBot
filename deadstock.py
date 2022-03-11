1055ab4e1a2ac7147f66d0b3e12e2e8a64b86aec
seb <sebyzy@gmail.com>
2022-03-11 07:11:12 -0500

    3.11

--- deadstock.py
+++ deadstock.py
@@ -76,6 +76,29 @@ def deadstock_early_link_mode(driver, link_to_run, size):
     #         driver.get(link_to_run)
 
 
+
+
+    # while boo:
+    #     try:
+    #         start1 = time.time()
+    #         click_size = driver.find_element_by_id("ProductSelect-option-Size-"+str(size))
+    #         ActionChains(driver).move_to_element(click_size).click(click_size).perform()
+    #         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "AddToCart"))).click()
+    #         if driver.find_element_by_xpath("//button[normalize-space()='Got it']"):
+    #             driver.find_element_by_xpath("//button[normalize-space()='Got it']").click()
+    #         #WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//button[normalize-space()='Got it']"))).click()
+    #         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//a[normalize-space()='View Cart']"))).click()
+    #         boo = False
+    #         print("carted: \n"+"--- %f seconds ---" % (time.time() - start1))
+    #         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//button[normalize-space()='Check Out']"))).click()
+    #         start2 = time.time()
+    #         # WebDriverWait(driver, 30).until(EC.visibility_of_element_located(
+    #         #     (By.XPATH, "//span[normalize-space()='Pay now']"))).click()
+    #         if WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//span[normalize-space()='Pay now']"))):
+    #             print("checked out: \n"+"--- %f seconds ---" % (time.time() - start2))
+    #     except:
+    #         driver.get(link_to_run)
+
 def deadstock_safe_mode(driver, keywords, size):
     driver.get(UPCOMING_RELEASES)
     boo = True
@@ -126,7 +149,7 @@ def deadstock_main(PATH, PROFILE_PATH, KEYWORDS, SIZE, SAFE_MODE):
 
 PATH = "/Users/seb/Chromedriver/chromedriver"
 PROFILE_PATH = "/Users/seb/Library/Application Support/Google/Chrome/Default"
-KEYWORDS = "adidas Originals Kids’ Yeezy Foam Runner / Mist"
+KEYWORDS = "adidas Originals Kids’ Yeezy Foam Runner / Stone Sage"
 SIZE = "12k"
 SAFE_MODE = False
 
